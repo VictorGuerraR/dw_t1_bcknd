f36c31ce3d56de9cc2a6bfbaf0a7239a94963245
VictorGuerraR <victorhgrodriguez@gmail.com>
2023-08-24 11:57:28 -0600

    [ADD] AHORA SI YA FUNCIONA...

--- tarea1/ejemplos.js
+++ tarea1/ejemplos.js
@@ -1,22 +1,10 @@
 import db from '../db_conection/db_conection.js'
 
-export const get_algo = async (req, res) => {
+export const informacionEstudiantes = async (req, res) => {
   try {
     let result = await db({ e: 'db_empresa.estudiantes' })
       .innerJoin({ ts: 'db_empresa.tipos_sangre' },
-        'e.id_tipo_sangre',
-        'ts.id_tipo_sangre'
-      )
-      .select(
-        'e.id_estudiante as Numero',
-        'e.carne as carné',
-        'e.nombres',
-        'e.apellidos',
-        'e.direccion',
-        'e.telefono',
-        'e.correo_electronico as correo',
-        'e.fecha_nacimiento as fecha nacimiento',
-        'ts.sangre as Tipo de Sangre',
+        'e.id_tipo_sangre', 'ts.id_tipo_sangre'
       )
     res.status(200).render('../views/index', { estudiantes: result });
   } catch (error) {
@@ -24,7 +12,7 @@ export const get_algo = async (req, res) => {
   }
 }
 
-export const get_add_estudiante = async (req, res) => {
+export const formularioAddEstudiante = async (req, res) => {
   try {
     let result = await db('db_empresa.tipos_sangre')
     res.status(200).render('../views/add', { tiposDeSangre: result });
@@ -33,24 +21,58 @@ export const get_add_estudiante = async (req, res) => {
   }
 }
 
-export const post_algo = async (req, res) => {
+export const endpointEstudiante = async (req, res) => {
   try {
-    let { carne, nombres, apellidos, direccion, telefono, correo_electronico, id_tipo_sangre, fecha_nacimiento } = req.body;
-    let valores = { carne, nombres, apellidos, direccion, telefono, correo_electronico, id_tipo_sangre, fecha_nacimiento }
-    await db('db_empresa.estudiantes')
-      .insert(valores, ['id_estudiante'])
+    let idEstudiante;
+    if (req.body?.create) {
+      idEstudiante = createEstudianteFun(req.body)
+    } else if (req.body?.update) {
+      idEstudiante = updateEstudianteFun(req.body)
+    } else if (req.body?.delete) {
+      idEstudiante = deleteEstudianteFun(req.body)
+    }
+
     res.status(200).redirect('/')
   } catch (error) {
     res.status(500).redirect('/')
   }
 }
 
-export const patch_algo = async (req, res) => {
-  console.log('patch')
-
+const createEstudianteFun = async (informacion) => {
+  try {
+    await db.transaction(async trx => {
+      let { carne, nombres, apellidos, direccion, telefono, correo_electronico, id_tipo_sangre, fecha_nacimiento } = informacion;
+      let valores = { carne, nombres, apellidos, direccion, telefono, correo_electronico, id_tipo_sangre, fecha_nacimiento }
+      return await trx('db_empresa.estudiantes')
+        .insert(valores, ['id_estudiante'])
+    })
+  } catch (error) {
+    throw error;
+  }
 }
 
-export const delete_algo = async (req, res) => {
-  console.log('delete')
+const updateEstudianteFun = async (informacion) => {
+  try {
+    await db.transaction(async trx => {
+      let { carne, nombres, apellidos, direccion, telefono, correo_electronico, id_tipo_sangre, fecha_nacimiento } = informacion;
+      let valores = { carne, nombres, apellidos, direccion, telefono, correo_electronico, id_tipo_sangre, fecha_nacimiento }
+      return await trx('db_empresa.id_estudiante')
+        .update(valores, 'id_estudiante')
+        .where('id_estudiante', informacion.id_estudiante)
+    })
+  } catch (error) {
+    throw error;
+  }
+}
 
+const deleteEstudianteFun = async (informacion) => {
+  try {
+    await db.transaction(async trx => {
+      return await trx('db_empresa.estudiantes')
+        .delete()
+        .where('id_estudiante', informacion.id_estudiante)
+    })
+  } catch (error) {
+    throw error;
+  }
 }
--- tarea1/route.js
+++ tarea1/route.js
@@ -3,11 +3,8 @@ import * as ejemplos from './ejemplos.js'
 
 const routes = new Router()
 
-routes.get('/', ejemplos.get_algo)
-routes.get('/add', ejemplos.get_add_estudiante)
-routes.post('/add', ejemplos.post_algo)
-routes.patch('/', ejemplos.patch_algo)
-routes.delete('/', ejemplos.delete_algo)
-
+routes.get('/', ejemplos.informacionEstudiantes)
+routes.get('/add', ejemplos.formularioAddEstudiante)
+routes.post('/add', ejemplos.endpointEstudiante)
 
 export default routes;
